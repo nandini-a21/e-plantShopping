714f9340e1bb62604b32947a96d1677f1e6b8751
nandini-a21 <nandinia764@gmail.com>
2025-07-04 20:55:17 -0400

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import  addItem  from './CartSlice.jsx';
+import { useDispatch } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -252,7 +254,9 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+    const dispatch=useDispatch();
     const handleAddToCart = (product) => {
+        
   dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
 
   setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
